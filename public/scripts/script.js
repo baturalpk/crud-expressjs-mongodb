c3ebd47637eb7707cb925e080adec71deb151e47
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2020-12-31 01:22:32 +0300

    Add frontend js script

---
+++ public/scripts/script.js
@@ -0,0 +1,29 @@
+
+/**
+ * DEPRECATED for the time being..!
+*/
+
+/*
+const update_name = document.querySelector('#change-name-button')
+
+update_name.addEventListener('click', _ => {
+    fetch('crud', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify({
+            name: "TEXT",
+            surname: "TEXT2"            
+        }) 
+    })
+    console.log("work-test");
+})
+
+const update_surname = document.querySelector('#change-surname-button')
+
+update_surname.addEventListener('click', _ => {
+    fetch('crud', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'}
+    })
+})
+*/
