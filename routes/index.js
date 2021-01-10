b5b688531ee63fe0aec9f6cc966a12fc0a221aa0
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-01-10 19:51:01 +0300

    Add visitors' IP address to the welcome message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'A test environment for basic CRUD operations...' });
+  res.render('index', { title: 'A test environment for basic CRUD operations...', ip: req.ip.substr(7) });
 });
 
 module.exports = router;
