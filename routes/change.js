576dd5357501ec714ce8c25572059740bf507eb3
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-01-11 17:00:42 +0300

    Add new env. variables & Fix abusive-word filter

--- routes/change.js
+++ routes/change.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
+// Environment variables
 require('dotenv').config();
 const URI = process.env.DB_URI;
+const DB = process.env.DB_NAME;
+const COLLECTION = process.env.DB_COLLECTION;
 const dirtyWords = process.env.DIRTY_WORDLIST.split(',');
 
 router.get('/', (req, res, next) => {
@@ -13,15 +16,17 @@ router.post('/', (req, res, next) => {
     var data = req.body;
     const client = new MongoClient(URI, { useNewUrlParser: true , useUnifiedTopology: true});
     client.connect(err => {
-        const collection = client.db("test").collection("quotes");
+        const collection = client.db(DB).collection(COLLECTION);
 
         // Check for abusive words
         dirtyWords.forEach(word => {
-            if (data.name.includes(word))
-                data.name = "******";
-
-            if (data.surname.includes(word))
-                data.surname = "******";
+            if (Object.prototype.hasOwnProperty.bind(data)('old_name')) 
+                if (data.new_name.includes(word))
+                    data.new_name = "******";
+            
+            if (Object.prototype.hasOwnProperty.bind(data)('old_surname'))
+                if (data.new_surname.includes(word))
+                    data.new_surname = "******";
         });
     
         // Determine and update desired name value
